96b4b8efe540e9253bc8f6bcfe6abeb525007997
Zach Bachar <zachbachar@gmail.com>
2019-01-05 17:56:09 +0200

    storyboard global architecture

---
+++ GoldenHour/ViewControllers/RegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterViewController.swift
+//  GoldenHour
+//
+//  Created by Zach Bachar on 05/01/2019.
+//  Copyright © 2019 Zach Bachar. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
