d5409f410a2b21145ba2639cf62a235f0f6b0731
segaldaniel <dannysegal1@gmail.com>
2019-01-12 23:33:15 +0200

    Created the second db using sql

--- GoldenHour/Model/ModelSql.swift
+++ GoldenHour/Model/ModelSql.swift
@@ -10,4 +10,28 @@ import Foundation
 import SQLite
 class ModelSql{
     
+    var dbSql: OpaquePointer? = nil
+    init() {
+        let dbFileName = "databaseSql.db"
+        if let dir = FileManager.default.urls(for: .documentDirectory, in:
+            .userDomainMask).first{
+            let path = dir.appendingPathComponent(dbFileName)
+            if sqlite3_open(path.absoluteString, &dbSql) != SQLITE_OK {
+                print("Failed to open db file: \(path.absoluteString)")
+                return
+            }
+            
+            CreateTable()
+        }
+    }
+    
+    func CreateTable(){
+        
+        
+    }
+    func dropTable(){
+        
+    }
+    
+    
 }
