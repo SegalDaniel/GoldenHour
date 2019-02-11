399425b8ddc380fa9e46fc7c828340d6c28cba90
segaldaniel <dannysegal1@gmail.com>
2019-02-11 16:56:00 +0200

    Bridging Header created

---
+++ GoldenHour-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GoldenHour-Bridging-Header.h
+//  GoldenHour
+//
+//  Created by Daniel Segal on 11/02/2019.
+//  Copyright © 2019 Zach Bachar. All rights reserved.
+//
+
+#ifndef GoldenHour_Bridging_Header_h
+#define GoldenHour_Bridging_Header_h
+
+#import <sqlite3.h>
+
+#endif /* GoldenHour_Bridging_Header_h */
