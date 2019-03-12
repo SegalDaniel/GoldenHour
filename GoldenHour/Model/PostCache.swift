56bd19ec9158fc8aee22f6a9dc787fcc6e63581a
segaldaniel <dannysegal1@gmail.com>
2019-03-12 12:48:59 +0200

    Started handling the getCache method, still having problems with getting posts

--- GoldenHour/Model/PostCache.swift
+++ GoldenHour/Model/PostCache.swift
@@ -43,4 +43,25 @@ extension Model{
             })
         }
     }
+    
+    func getCache(completion: @escaping ([(Post, User)]?)->Void) {
+        
+        /* Gets Posts and users local data */
+        var feedData = [(Post, User)]()
+        CacheHandler.cache.get(name: "POSTS", onSuccess: { (postsAsString : Array<[String]>) in
+      //      let posts = //need to pull posts and deal with jasons
+            CacheHandler.cache.get(name: "USERS", onSuccess: { (usersAsString: Array<[String]>) in
+           //     let users = //need to pull posts and deal with jasons
+                
+                /* Insert post and corresponding user into FeedData */
+//                for post in posts {
+//                    let user = users.first { $0?.userID == post?.userID }
+//                    guard let post = post else { return }
+//                    guard let correspondUser = user else { return }
+//                    feedData.append((post, correspondUser!))
+//                }
+                completion(feedData)
+            },onError: { completion(nil) })
+        },onError: { completion(nil) })
+    }
 }
